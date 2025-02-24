ab043758f7644e37d2e27416d5c5fe063d6caf67
Sukhovoy Alexey <a.sukhovoy@dpt-pro.ru>
2025-02-24 10:01:59 +0700

    Подготовка в InjectionPlace

---
+++ src/Modules/AngularMapLibreGLModule/Widgets/WidgetOption/WidgetOption.ts
@@ -0,0 +1,7 @@
+/* В базовых настройках для плагина есть только Id */
+export default class BaseWidgetOption {
+  Id: string;
+  constructor(Id: string) {
+    this.Id = Id;
+  }
+}
---
+++ src/Modules/AngularMapLibreGLModule/Widgets/WidgetOption/WidgetOptionTypes.ts
@@ -0,0 +1,3 @@
+import { ViewContainerRef } from "@angular/core";
+/*Место для встраивания компонентов */
+export type InjectionPlace = { Id: string; Container: ViewContainerRef };
