505e72be77dce2422bb1b037b80ea7d6a73f9e02
Sukhovoy Alexey <a.sukhovoy@dpt-pro.ru>
2025-03-09 17:35:27 +0700

    Добавлен компонент с переключателями для виджета проектов бурения

--- src/Modules/AngularMapLibreGLModule/Widgets/DrillingProjectsWidgetComponent/DrillingProjectsWidgetComponent.ts
+++ src/Modules/AngularMapLibreGLModule/Widgets/DrillingProjectsWidgetComponent/DrillingProjectsWidgetComponent.ts
@@ -1,12 +1,14 @@
 import { ChangeDetectionStrategy, Component } from "@angular/core";
 import BaseWidgetComponent from "../BaseWidgetComponent/BaseWidgetComponent";
 import TitlePanelComponent from "../../Components/TitlePanelComponent/TitlePanelComponent";
+import DrillingProjectsWidgetSwitchersComponent from "../../Components/DrillingProjectsWidgetSwitchersComponent/DrillingProjectsWidgetSwitchersComponent";
+
 @Component({
   selector: "DrillingProjectsWidgetComponent",
   templateUrl: "DrillingProjectsWidgetComponent.html",
   styleUrls: ["./DrillingProjectsWidgetComponent.css"],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [TitlePanelComponent],
+  imports: [TitlePanelComponent, DrillingProjectsWidgetSwitchersComponent],
 })
 export default class DrillingProjectsWidgetComponent extends BaseWidgetComponent {
   override Id: string = "DrillingProjects";
