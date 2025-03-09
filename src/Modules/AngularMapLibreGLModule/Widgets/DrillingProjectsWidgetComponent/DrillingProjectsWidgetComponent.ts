d1eec4e1cb3252db760962c2302f41f46f9e9d61
Sukhovoy Alexey <a.sukhovoy@dpt-pro.ru>
2025-03-09 19:27:14 +0700

    Обновлена верстка проектов бурения

--- src/Modules/AngularMapLibreGLModule/Widgets/DrillingProjectsWidgetComponent/DrillingProjectsWidgetComponent.ts
+++ src/Modules/AngularMapLibreGLModule/Widgets/DrillingProjectsWidgetComponent/DrillingProjectsWidgetComponent.ts
@@ -2,13 +2,20 @@ import { ChangeDetectionStrategy, Component } from "@angular/core";
 import BaseWidgetComponent from "../BaseWidgetComponent/BaseWidgetComponent";
 import TitlePanelComponent from "../../Components/TitlePanelComponent/TitlePanelComponent";
 import DrillingProjectsWidgetSwitchersComponent from "../../Components/DrillingProjectsWidgetSwitchersComponent/DrillingProjectsWidgetSwitchersComponent";
+import { NzSelectComponent } from "ng-zorro-antd/select";
+import { NzSwitchComponent } from "ng-zorro-antd/switch";
 
 @Component({
   selector: "DrillingProjectsWidgetComponent",
   templateUrl: "DrillingProjectsWidgetComponent.html",
   styleUrls: ["./DrillingProjectsWidgetComponent.css"],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [TitlePanelComponent, DrillingProjectsWidgetSwitchersComponent],
+  imports: [
+    TitlePanelComponent,
+    DrillingProjectsWidgetSwitchersComponent,
+    NzSelectComponent,
+    NzSwitchComponent,
+  ],
 })
 export default class DrillingProjectsWidgetComponent extends BaseWidgetComponent {
   override Id: string = "DrillingProjects";
