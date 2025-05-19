fc3f877e2ee2dce7120289cae4112fccbc21aee5
Sukhovoy Alexey <a.sukhovoy@dpt-pro.ru>
2025-05-19 16:29:53 +0700

    Обновление слоя для отображения 3D моделей

--- src/Modules/AngularMapLibreGLModule/LayerComponents/SimpleMeshLayerDirective/SimpleMeshLayerDirective.ts
+++ src/Modules/AngularMapLibreGLModule/LayerComponents/SimpleMeshLayerDirective/SimpleMeshLayerDirective.ts
@@ -1,17 +1,26 @@
 import { SimpleMeshLayer } from "@deck.gl/mesh-layers";
 import BaseLayerComponent from "../BaseLayerDirective/BaseLayerDirective";
-import { Directive } from "@angular/core";
+import { Directive, Input } from "@angular/core";
 import { OBJLoader } from "@loaders.gl/obj";
 /*Слой для отображения статичных моделей */
 @Directive({
   selector: "SimpleMeshLayerDirective",
 })
 export default class SimpleMeshLayerDirective extends BaseLayerComponent<SimpleMeshLayer> {
+  @Input()
+  SizeScale: number = 1;
+  @Input()
+  Mesh!: string;
+  @Input()
+  Data: any;
   override PrepareLayer(): void {
     this.Layer = new SimpleMeshLayer({
       id: this.Id,
       pickable: this.Pickable,
       loaders: [OBJLoader],
+      sizeScale: this.SizeScale,
+      mesh: this.Mesh,
+      data: this.Data,
     });
   }
 }
