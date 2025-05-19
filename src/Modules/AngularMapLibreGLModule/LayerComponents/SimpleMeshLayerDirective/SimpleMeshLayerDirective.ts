46142fc22d87b81c755b37e99e1d5dbd5f0e728e
Sukhovoy Alexey <a.sukhovoy@dpt-pro.ru>
2025-05-19 16:13:59 +0700

    Добавлен OBJ лоадер для отображения 3D моделей

--- src/Modules/AngularMapLibreGLModule/LayerComponents/SimpleMeshLayerDirective/SimpleMeshLayerDirective.ts
+++ src/Modules/AngularMapLibreGLModule/LayerComponents/SimpleMeshLayerDirective/SimpleMeshLayerDirective.ts
@@ -1,10 +1,17 @@
 import { SimpleMeshLayer } from "@deck.gl/mesh-layers";
 import BaseLayerComponent from "../BaseLayerDirective/BaseLayerDirective";
 import { Directive } from "@angular/core";
+import { OBJLoader } from "@loaders.gl/obj";
 /*Слой для отображения статичных моделей */
 @Directive({
   selector: "SimpleMeshLayerDirective",
 })
 export default class SimpleMeshLayerDirective extends BaseLayerComponent<SimpleMeshLayer> {
-  override PrepareLayer(): void {}
+  override PrepareLayer(): void {
+    this.Layer = new SimpleMeshLayer({
+      id: this.Id,
+      pickable: this.Pickable,
+      loaders: [OBJLoader],
+    });
+  }
 }
