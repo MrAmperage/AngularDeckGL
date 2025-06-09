bd406bf37e93c5f7d4d83f58127021cbc87a1dbc
Sukhovoy Alexey <a.sukhovoy@dpt-pro.ru>
2025-06-09 16:43:41 +0700

    Добавил возможность обработки загрузки тайлов в TerrainLayer через расширение

--- src/Modules/AngularMapLibreGLModule/Extensions/TerrainMeshExtension/TerrainMeshExtension.ts
+++ src/Modules/AngularMapLibreGLModule/Extensions/TerrainMeshExtension/TerrainMeshExtension.ts
@@ -1,6 +1,7 @@
 import { Layer, LayerContext, LayerExtension } from "@deck.gl/core";
 import { TerrainMeshExtensionProps } from "./TerrainMeshExtensionTypes";
 import { TerrainLayer } from "@deck.gl/geo-layers";
+import { Tile2DHeader } from "@deck.gl/geo-layers/dist/tileset-2d";
 
 /*Расширение  для слоев отображения 3D моделей на Terrain */
 export default class TerrainMeshExtension extends LayerExtension {
@@ -18,10 +19,32 @@ export default class TerrainMeshExtension extends LayerExtension {
   }
   /*Расширяем TerrainLayer и заново его инициализируем*/
   ReInitTerrainLayer(Layer: Layer, context: LayerContext, extension: this) {
-    const TerrainLayerInstance = context.deck?.props.layers.find((Layer) => {
-      return (
-        Layer instanceof TerrainLayer && Layer.id === extension.TerrainLayerId
+    if (context.deck !== undefined) {
+      const TerrainLayerIndex = context.deck?.props.layers.findIndex(
+        (Layer) => {
+          return (
+            Layer instanceof TerrainLayer &&
+            Layer.id === extension.TerrainLayerId
+          );
+        }
       );
-    });
+
+      if (TerrainLayerIndex !== -1) {
+        const NewTerrainLayerInstance = (
+          context.deck.props.layers[TerrainLayerIndex] as TerrainLayer
+        ).clone({
+          onTileLoad(Tile) {
+            extension.TileHandler(Tile);
+          },
+        });
+        const NewLayers = context.deck.props.layers;
+        NewLayers[TerrainLayerIndex] = NewTerrainLayerInstance;
+        context.deck.setProps({ layers: NewLayers });
+      }
+    }
   }
+
+  TileHandler = (Tile: Tile2DHeader) => {
+    console.log(444, Tile);
+  };
 }
