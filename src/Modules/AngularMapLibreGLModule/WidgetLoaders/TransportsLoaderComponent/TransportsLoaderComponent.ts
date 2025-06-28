544627b59112d2327a50875d5336bb5068e3393d
Sukhovoy Alexey <a.sukhovoy@dpt-pro.ru>
2025-06-28 18:59:29 +0700

    Обновлен инструмент транспорта

--- src/Modules/AngularMapLibreGLModule/WidgetLoaders/TransportsLoaderComponent/TransportsLoaderComponent.ts
+++ src/Modules/AngularMapLibreGLModule/WidgetLoaders/TransportsLoaderComponent/TransportsLoaderComponent.ts
@@ -4,4 +4,23 @@ import BaseLoaderComponent from "../BaseLoaderComponent/BaseLoaderComponent";
   selector: "TransportsLoaderComponent",
   templateUrl: "TransportsLoaderComponent.svg",
 })
-export default class TransportsLoaderComponent extends BaseLoaderComponent {}
+export default class TransportsLoaderComponent extends BaseLoaderComponent {
+  override ClickOnLoader(): void {
+    import(
+      "../../Widgets/TransportsWidgetComponent/TransportsWidgetComponent"
+    ).then((Widget) => {
+      super.ClickOnLoader();
+
+      if (this.GetInternalWidgetOption?.IsShow) {
+        Widget.default;
+        this.ToolbarWidgetComponent.AddWidgetComponent(Widget.default);
+      } else {
+        if (this.GetInternalWidgetOption !== undefined) {
+          this.ToolbarWidgetComponent.RemoveWidgetComponentById(
+            this.GetInternalWidgetOption.Id
+          );
+        }
+      }
+    });
+  }
+}
