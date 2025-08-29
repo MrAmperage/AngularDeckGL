86f9c5b14d33b8044b56be131eaf42c5d11f468b
Sukhovoy Alexey <a.sukhovoy@dpt-pro.ru>
2025-08-29 11:02:34 +0700

    Обновлены зависимости и импорты

--- src/public-api.ts
+++ src/public-api.ts
@@ -1,35 +1,35 @@
 /*Ядро */
-export { default as DeckGLComponent } from "./Modules/AngularMapLibreGLModule/LayerComponents/DeckGLComponent/DeckGLComponent";
+export { default as DeckGLComponent } from "./Modules/AngularDeckGLModule/LayerComponents/DeckGLComponent/DeckGLComponent";
 
 /*Оболочки слоев */
-export { default as TileLayerDirective } from "./Modules/AngularMapLibreGLModule/LayerComponents/TileLayerDirective/TileLayerDirective";
-export { default as TerrainLayerDirective } from "./Modules/AngularMapLibreGLModule/LayerComponents/TerrainLayerDirective/TerrainLayerDirective";
-export { default as SimpleMeshLayerDirective } from "./Modules/AngularMapLibreGLModule/LayerComponents/SimpleMeshLayerDirective/SimpleMeshLayerDirective";
+export { default as TileLayerDirective } from "./Modules/AngularDeckGLModule/LayerComponents/TileLayerDirective/TileLayerDirective";
+export { default as TerrainLayerDirective } from "./Modules/AngularDeckGLModule/LayerComponents/TerrainLayerDirective/TerrainLayerDirective";
+export { default as SimpleMeshLayerDirective } from "./Modules/AngularDeckGLModule/LayerComponents/SimpleMeshLayerDirective/SimpleMeshLayerDirective";
 
 /*Виджеты */
-export { default as DrillingProjectsWidgetComponent } from "./Modules/AngularMapLibreGLModule/WidgetComponents/DrillingProjectsWidgetComponent/DrillingProjectsWidgetComponent";
-export { default as ToolbarWidgetComponent } from "./Modules/AngularMapLibreGLModule/WidgetComponents/ToolbarWidgetComponent/ToolbarWidgetComponent";
-export { default as TerrainWidgetComponent } from "./Modules/AngularMapLibreGLModule/WidgetComponents/TerrainWidgetComponent/TerrainWidgetComponent";
+export { default as DrillingProjectsWidgetComponent } from "./Modules/AngularDeckGLModule/WidgetComponents/DrillingProjectsWidgetComponent/DrillingProjectsWidgetComponent";
+export { default as ToolbarWidgetComponent } from "./Modules/AngularDeckGLModule/WidgetComponents/ToolbarWidgetComponent/ToolbarWidgetComponent";
+export { default as TerrainWidgetComponent } from "./Modules/AngularDeckGLModule/WidgetComponents/TerrainWidgetComponent/TerrainWidgetComponent";
 
 /*Лоадеры */
-export { default as DrillingProjectsLoaderComponent } from "./Modules/AngularMapLibreGLModule/WidgetLoaders/DrillingProjectsLoaderComponent/DrillingProjectsLoaderComponent";
-export { default as TransportsLoaderComponent } from "./Modules/AngularMapLibreGLModule/WidgetLoaders/TransportsLoaderComponent/TransportsLoaderComponent";
-export { default as TerrainLoaderComponent } from "./Modules/AngularMapLibreGLModule/WidgetLoaders/TerrainLoaderComponent/TerrainLoaderComponent";
+export { default as DrillingProjectsLoaderComponent } from "./Modules/AngularDeckGLModule/WidgetLoaders/DrillingProjectsLoaderComponent/DrillingProjectsLoaderComponent";
+export { default as TransportsLoaderComponent } from "./Modules/AngularDeckGLModule/WidgetLoaders/TransportsLoaderComponent/TransportsLoaderComponent";
+export { default as TerrainLoaderComponent } from "./Modules/AngularDeckGLModule/WidgetLoaders/TerrainLoaderComponent/TerrainLoaderComponent";
 
 /*Сервисы */
-export { default as MapService } from "./Modules/AngularMapLibreGLModule/Services/MapService/MapService";
+export { default as MapService } from "./Modules/AngularDeckGLModule/Services/MapService/MapService";
 
 /*Контролеры */
-export { default as CustomMapController } from "./Modules/AngularMapLibreGLModule/LayerComponents/DeckGLComponent/CustomMapController/CustomMapController";
+export { default as CustomMapController } from "./Modules/AngularDeckGLModule/LayerComponents/DeckGLComponent/CustomMapController/CustomMapController";
 
 /*Расширения */
-export { default as TerrainMeshExtension } from "./Modules/AngularMapLibreGLModule/Extensions/TerrainMeshExtension/TerrainMeshExtension";
+export { default as TerrainMeshExtension } from "./Modules/AngularDeckGLModule/Extensions/TerrainMeshExtension/TerrainMeshExtension";
 
 /*Оболочки расширений */
-export { default as TerrainMeshExtensionDirective } from "./Modules/AngularMapLibreGLModule/Extensions/TerrainMeshExtensionDirective/TerrainMeshExtensionDirective";
+export { default as TerrainMeshExtensionDirective } from "./Modules/AngularDeckGLModule/Extensions/TerrainMeshExtensionDirective/TerrainMeshExtensionDirective";
 
 /*Модели*/
-export { default as MapModel } from "./Modules/AngularMapLibreGLModule/AbstractionModels/MapModel/MapModel";
+export { default as MapModel } from "./Modules/AngularDeckGLModule/AbstractionModels/MapModel/MapModel";
 
 /*Лоадеры для слоев*/
-export { default as BaseLayerLoader } from "./Modules/AngularMapLibreGLModule/LayerLoaders/BaseLayerLoader/BaseLayerLoader";
+export { default as BaseLayerLoader } from "./Modules/AngularDeckGLModule/LayerLoaders/BaseLayerLoader/BaseLayerLoader";
