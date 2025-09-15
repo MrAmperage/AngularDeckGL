266f0fc2729a81d29f9034d6b51270ed4f0d044c
Sukhovoy Alexey <a.sukhovoy@dpt-pro.ru>
2025-09-15 10:24:18 +0700

    Обновлен SimpleMeshLayerDirective

--- src/Modules/AngularDeckGLModule/LayerComponents/SimpleMeshLayerDirective/SimpleMeshLayerDirective.ts
+++ src/Modules/AngularDeckGLModule/LayerComponents/SimpleMeshLayerDirective/SimpleMeshLayerDirective.ts
@@ -1,13 +1,17 @@
 import { SimpleMeshLayer } from "@deck.gl/mesh-layers";
 import BaseLayerDirective from "../BaseLayerDirective/BaseLayerDirective";
-import { Directive, Input } from "@angular/core";
+import { Directive, Inject, Input } from "@angular/core";
 import { MapModelOptions } from "../../AbstractionModels/MapModel/MapModelTypes";
+import DeckGLComponent from "../DeckGLComponent/DeckGLComponent";
 
 /*Слой для отображения статичных моделей */
 @Directive({
   selector: "SimpleMeshLayerDirective",
 })
 export default class SimpleMeshLayerDirective extends BaseLayerDirective<SimpleMeshLayer> {
+  constructor(@Inject(DeckGLComponent) DeckGLComponent: DeckGLComponent) {
+    super(DeckGLComponent);
+  }
   @Input()
   GetPosition = (MapModel: MapModelOptions) => {
     return MapModel.Coordinates;
